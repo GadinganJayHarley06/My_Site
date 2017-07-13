8039bd686283998d6af921d0b1e577b44a0618a8
Jay Harley Gadingan <Jay Harley Gadingan>
2017-07-14 01:41:22 +0700

    Error

---
+++ polls/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
